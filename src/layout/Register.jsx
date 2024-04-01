82a5b0109e8ff33f1eb4f2dafce2259839e47d32
mozzammelrobi <mozzammelfb@gmail.com>
2024-04-01 08:26:26 +0600

    daisyui login form

--- src/layout/Register.jsx
+++ src/layout/Register.jsx
@@ -1,9 +1,75 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(name,email, password)
+    }
+
     return (
-        <div>
-            <h1 className="text-3xl">Register</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center ">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div>
+                        <form
+                            onSubmit={handleRegister}
+                            className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    name='name'
+                                    placeholder="name"
+                                    className="input input-bordered"
+                                    required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    name='email'
+                                    placeholder="email"
+                                    className="input input-bordered"
+                                    required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+
+                                    type="password"
+                                    name='password'
+                                    placeholder="password"
+                                    className="input input-bordered"
+                                    required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                        <p>Alredy have account? please<Link to='/login'>
+                            <button className='btn btn-link'>Login</button>
+                        </Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
